741a462c7360f0de0ab9840b57407ffb3aec579c
tubadan33 <daniel.herrera33@proton.me>
2025-04-23 19:55:25 -0600

    2. Remove arg length constraint

--- src/main/java/src/Main.java
+++ src/main/java/src/Main.java
@@ -8,8 +8,6 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 
-        Utils.argsLenCheck(input);
-
         List<Integer> parsed = Utils.parseInput(input);
         System.out.println(Utils.sum(parsed));
     }
