84ede9b510450f208b18eb560846a99e1a45bbb3
tubadan33 <daniel.herrera33@proton.me>
2025-04-24 19:35:45 -0600

    Stretch Goal - Allow input until user disconnects from vm

--- src/main/java/src/Main.java
+++ src/main/java/src/Main.java
@@ -5,14 +5,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        boolean running = true;
+        while (running) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                String input = scanner.nextLine();
 
-        List<Integer> parsed = Utils.parseInput(input);
-        Utils.negativeNumberCheck(parsed);
-        int sum = Utils.sum(parsed);
-        Utils.printFormula(parsed, sum);
-        System.out.println(sum);
+                List<Integer> parsed = Utils.parseInput(input);
+                Utils.negativeNumberCheck(parsed);
+                int sum = Utils.sum(parsed);
+                Utils.printFormula(parsed, sum);
+                System.out.println(sum);
+            } catch (Exception e) {
+                running = false;
+            }
+        }
     }
 }
 
