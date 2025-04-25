f910a2e5f24ff41e6c7df07dca219596939e83bc
tubadan33 <daniel.herrera33@proton.me>
2025-04-24 21:45:16 -0600

    Revert "Stretch Goal - Allow input until user disconnects from vm"
    This reverts commit 84ede9b510450f208b18eb560846a99e1a45bbb3.

--- src/main/java/src/Main.java
+++ src/main/java/src/Main.java
@@ -5,21 +5,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        boolean running = true;
-        while (running) {
-            try {
-                Scanner scanner = new Scanner(System.in);
-                String input = scanner.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
 
-                List<Integer> parsed = Utils.parseInput(input);
-                Utils.negativeNumberCheck(parsed);
-                int sum = Utils.sum(parsed);
-                Utils.printFormula(parsed, sum);
-                System.out.println(sum);
-            } catch (Exception e) {
-                running = false;
-            }
-        }
+        List<Integer> parsed = Utils.parseInput(input);
+        Utils.negativeNumberCheck(parsed);
+        int sum = Utils.sum(parsed);
+        Utils.printFormula(parsed, sum);
+        System.out.println(sum);
     }
 }
 
